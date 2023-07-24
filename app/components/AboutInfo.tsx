3da176a918ed19eefc5ada77112651b4b66bd5b8
davijura <davijura88@gmail.com>
2023-07-24 17:08:26 +0200

    HomePage respo fix

--- app/components/AboutInfo.tsx
+++ app/components/AboutInfo.tsx
@@ -1,16 +1,16 @@
 
 export default function AboutInfo() {
     return (
-        <div className="grid grid-cols-1 mx-auto md:grid-cols-2 gap-20 pb-28 items-center justify-items-center px-4 md:px-0 max-w-max">
+        <div className="grid grid-cols-1 mx-auto md:grid-cols-2 gap-20 pb-24 items-center justify-items-center px-4 md:px-0 max-w-max">
 
-            <div className="md:order-1 w-[450px]">
-                <p className='font-bold text-black text-2xl pb-5'><strong>Vítejte u Vašeho společníka v oblasti lesnictví, těžby dřeva a pěstebních činností.</strong></p>
+            <div className="md:order-1 max-w-[450px]">
+                <p className='font-bold text-black text-2xl pb-5 text-center sm:text-left'><strong>Vítejte u Vašeho společníka v oblasti lesnictví, těžby dřeva a pěstebních činností.</strong></p>
                 <p className="text-base sm:text-lg lg:text-xl text-black leading-relaxed">
                     Jsme mladá a dynamická společnost s hlubokou vášní a respektem ke krajině. Nacházíme se přímo v srdci přírody, kde máme možnost obdivovat nezměrnou krásu a hodnoty, které lesy přinášejí. Naše cíle a metody v oblasti těžby dřeva jsou navrženy tak, aby byly udržitelné a šetrné k přírodě.
                 </p>
             </div>
 
-            <div className="md:order-2 w-[500px] h-[370px]">
+            <div className="md:order-2 max-w-[500px] h-[370px]">
                 <img className="w-full h-full object-cover" src="/tree.jpg" alt="Logo" />
             </div>
 
--- app/components/AboutInfoThird.tsx
+++ app/components/AboutInfoThird.tsx
@@ -3,14 +3,14 @@ export default function AboutInfoThird() {
     return (
         <div className="grid grid-cols-1 mx-auto md:grid-cols-2 gap-20 pb-20 items-center justify-items-center px-4 md:px-0 max-w-max">
 
-            <div className="md:order-1 w-[450px]">
-                <p className='font-bold text-black text-2xl pb-5'><strong>Naše služby jsou šité na míru potřebám našich zákazníků.</strong></p>
+            <div className="md:order-1 max-w-[450px]">
+                <p className='font-bold text-black text-2xl pb-5 text-center sm:text-left'><strong>Naše služby jsou šité na míru potřebám našich zákazníků.</strong></p>
                 <p className="text-base sm:text-lg lg:text-xl text-black leading-relaxed">
                     Ať už jste majitelem lesního pozemku, výrobcem dřeva nebo jiným partnerem v lesnickém odvětví, nabízíme vám profesionální a spolehlivé řešení. Sinien Koira, s. r. o. je synonymem pro kvalitu, důvěryhodnost a spolehlivost. Pokud máte zájem o naše služby nebo se chcete dozvědět více o naší společnosti, jsme připraveni vám pomoci a společně dosáhnout vašich cílů v oblasti lesnictví.
                 </p>
             </div>
 
-            <div className="md:order-2 w-[500px] h-[370px]">
+            <div className="md:order-2 max-w-[500px] h-[370px]">
                 <img className="w-full h-full object-cover" src="/chainsaw.jpg" alt="Logo" />
             </div>
 
