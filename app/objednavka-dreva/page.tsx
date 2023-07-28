cf763fdc4d85137ead7aa7a87f19d6f9542a16bf
davijura <davijura88@gmail.com>
2023-07-28 14:26:29 +0200

    Navbar fix

--- app/objednavka-dreva/page.tsx
+++ app/objednavka-dreva/page.tsx
@@ -7,7 +7,9 @@ export default function ObjednavkaDreva() {
     return (
         <div>
 
-            <NavbarAbove />
+            <div className='hidden sm:block'>
+                <NavbarAbove />
+            </div>
 
             <NewNavbar />
 
