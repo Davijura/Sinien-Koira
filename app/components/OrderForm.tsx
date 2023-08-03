e7858de7086ab25966964ae174bdadc86c059d3d
davijura <davijura88@gmail.com>
2023-08-03 02:25:47 +0200

    Netlify fix

--- app/components/OrderForm.tsx
+++ app/components/OrderForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { type FormEvent, useState } from 'react'
 import { motion } from 'framer-motion'
 import emailjs from '@emailjs/browser'
