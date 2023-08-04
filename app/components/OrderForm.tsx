1710ae3b193040e823772d1cf0faacaac920b3eb
davijura <davijura88@gmail.com>
2023-08-04 12:05:20 +0200

    Form text update

--- app/components/OrderForm.tsx
+++ app/components/OrderForm.tsx
@@ -105,7 +105,7 @@ export default function OrderForm (): JSX.Element {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-8 h-8">
               <path strokeLinecap="round" strokeLinejoin="round" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <span className='text-white'>Objednávka se nezdařila!</span>
+            <span className='text-white'>Vyplňte povinné údaje!</span>
           </div>
         </motion.div>
       )}
--- app/components/OrderInfo.tsx
+++ app/components/OrderInfo.tsx
@@ -47,7 +47,7 @@ export default function OrderInfo ({ personalData, onChange, isSubmissionFailed 
                     <input onChange={onChange} type="email" value={personalData.user_email} name="user_email" className={userEmailClass} />
                 </div>
 
-                <p className='text-gray-700 text-sm italic'><span className='text-red-500'>*</span> Povinné</p>
+                <p className='text-gray-700 text-sm italic'><span className='text-red-500'>*</span> Povinné údaje</p>
 
             </div>
             <div className="w-full md:w-3/5 lg:w-1/2 px-2" >
