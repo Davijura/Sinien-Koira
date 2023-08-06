25ba465cf09257ac151d5c60bfafc2015b20c59b
davijura <davijura88@gmail.com>
2023-08-06 21:36:23 +0200

    Test

--- app/components/Checkbox.tsx
+++ app/components/Checkbox.tsx
@@ -9,12 +9,16 @@ interface CheckboxProps {
 export default function Checkbox ({ isChecked, onChange }: CheckboxProps): JSX.Element {
   return (
 
-        <div className="form-control flex flex-col items-center justify-center sm:flex-row sm:items-center sm:justify-between pt-5">
-            <label className="label cursor-pointer flex items-center space-x-3  mb-5 sm:mb-0">
-                <input type="checkbox" checked={isChecked} onChange={onChange} className="text-blue-500 checkbox bg-white" />
-                <span className="label-text text-gray-700">Souhlasím se <Link href="/ochrana-osobnich-udaju" className="text-blue-500">zpracováním osobních údajů</Link></span>
-            </label>
-            <button type="submit" disabled={!isChecked} className="btn bg-green-500 hover:bg-green-600 border-none text-white" role="button" aria-disabled="true">Vytvořit poptávku</button>
-        </div>
+    <div className="form-control flex flex-col items-center justify-center sm:flex-row sm:items-center sm:justify-between pt-5">
+      <label className="label cursor-pointer flex items-center space-x-3  mb-5 sm:mb-0">
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={onChange}
+          className="text-blue-500 checkbox bg-white" />
+        <span className="label-text text-gray-700">Souhlasím se <Link href="/ochrana-osobnich-udaju" className="text-blue-500">zpracováním osobních údajů</Link></span>
+      </label>
+      <button type="submit" disabled={!isChecked} className="btn bg-green-500 hover:bg-green-600 border-none text-white" role="button" aria-disabled="true">Vytvořit poptávku</button>
+    </div>
   )
 }
--- app/objednavka-dreva/page.tsx
+++ app/objednavka-dreva/page.tsx
@@ -2,20 +2,25 @@
 import NewNavbar from '../components/NewNavbar'
 import OrderPage from '../components/OrderPage'
 import NavbarAbove from '../components/NavbarAbove'
+import Head from 'next/head'
 
 export default function ObjednavkaDreva (): JSX.Element {
   return (
-        <div>
 
-            <div className='hidden sm:block'>
-                <NavbarAbove />
-            </div>
+    <div>
+      <Head>
+        <title>Sinien Koira - Objednávka dřeva</title>
+      </Head>
 
-            <NewNavbar />
+      <div className='hidden sm:block'>
+        <NavbarAbove />
+      </div>
 
-            <OrderPage />
+      <NewNavbar />
 
-        </div>
+      <OrderPage />
+
+    </div>
 
   )
 }
