c3a9a6360a2636f14171ebc30d54511754610252
davijura <davijura88@gmail.com>
2023-08-14 14:57:25 +0200

    Fix

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,14 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document (): any {
-  return (
-    <Html lang="cs">
-      <Head />
-      <meta name = "theme-color" content = "#243b55"></meta>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render (): any {
+    return (
+      <Html lang="cs">
+        <Head>
+          <meta name="theme-color" content="#243b55" />
+          {/* Další meta tagy a styly můžete přidat zde */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
