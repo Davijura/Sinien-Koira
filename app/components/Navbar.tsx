4e2aa3ddc42826e7c41a170aed930c489d8296b1
davijura <davijura88@gmail.com>
2023-08-14 10:27:41 +0200

    Mobile navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -28,7 +28,7 @@ export default function Navbar (): JSX.Element {
         <div className={'drawer z-10'}>
             <input id="my-drawer-3" type="checkbox" className="drawer-toggle" checked={drawerOpen} onChange={toggleDrawer} />
             <div className="drawer-content">
-                <div className={`navbar flex justify-between h-28 lg:px-40 pt-5 px-10 z-20 fixed w-screen ${atTop ? 'bg-transparent' : ''}`}
+                <div className={`navbar flex justify-between h-24 md:h-28 lg:px-40  px-10 z-20 fixed ${atTop ? ' bg-green-600 md:bg-transparent' : ''}`}
                     style={atTop ? {} : { background: 'radial-gradient(circle at -1% 57.5%, rgb(19, 170, 82) 0%, rgb(0, 102, 43) 90%)', opacity: 0.9 }}>
                     <div className="navbar-start">
                         <Link href="/">
