9e7b9c643a50857b06f7025e641ce4de10a6d570
davijura <davijura88@gmail.com>
2023-08-15 11:47:31 +0200

    Color change

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
   description: 'Lesnictví a prodej palivového dřeva',
   keywords: 'dřevo, pila, traktor, objednávka, dřevopráce, lesnictví, les, vlečka, palivové, tvrdé, měkké, prodej, zelená, Sinien Koira, Čelechovice na Hané',
   viewport: 'device-width, initial-scale=1, shrink-to-fit=no',
-  themeColor: '#38a169'
+  themeColor: '#2f855a'
 }
 
 export default function RootLayout ({
